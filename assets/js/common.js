a5fd32cb93bfc3556161c1f4ee71248549315bd1
unknown <le.tc@dgwasia.com>
2024-03-28 15:59:03 +0700

    update index

--- assets/js/common.js
+++ assets/js/common.js
@@ -85,6 +85,36 @@ $(function () {
       $(".conversion,.contact,.pagetop").toggleClass("show", top > 90);
     });
   }
+
+  $('.slider').slick({
+    autoplay: true,
+    speed: 800,
+    lazyLoad: 'progressive',
+    arrows: true,
+    dots: true,
+  }).slickAnimation();
+
+  // grab all panel classes
+  const panels = document.querySelectorAll(".panel");
+
+  // loop through the node list
+  panels.forEach((panel) => {
+  // listen for a click
+  panel.addEventListener("click", () => {
+    removeActive();
+    // if clicked -> add a class of active (so also flex = 5)
+    panel.classList.add("active");
+  });
+  });
+
+  // function to remove active class from elements
+  function removeActive() {
+  // loop through panels
+  panels.forEach((panel) => {
+    // remove active classes from panel objects
+    panel.classList.remove("active");
+  });
+  }
 });
 
 function getHeight(parent, classname, column) {
